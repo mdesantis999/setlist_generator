f8c15663bc7b2ac7fbfa00641c7a467af35c10d3
mdesantis999 <michaeljdesantis@gmail.com>
2014-09-21 11:52:15 -0400

    updated the setlist profile
    update setlist profile to handle instances when the percentage of fast
    + med + slow < total number of songs

--- ordered_setlist.py
+++ ordered_setlist.py
@@ -22,30 +22,53 @@ random.shuffle(songs)
 
 num_setlists = input('Enter number of sets: ')
 
-num_setlists = num_setlists - 1
-
-
-#input number of songs per set    #python lists start at 0
+#input number of songs per set    
 
 num_songs = input('Enter number of songs per set: ')
 
-num_songs = num_songs - 1
-
 
 #initialize the setlist profile
+#TODO: separate this out into a function
+#based on total number of songs per set, figure out how
+#many are fast, medium or slow based on the percentages in profile
 
 profile = { 'fast': .40, 'medium': .30, 'slow': .30 }
 
-num_fast = num_songs * profile.get('fast')
+num_fast = int(num_songs * profile.get('fast'))
 
-num_medium = num_songs * profile.get('medium')
+num_medium = int(num_songs * profile.get('medium'))
 
-num_slow = num_songs * profile.get('slow')
-
-print int(num_fast), int(num_medium), int(num_slow)
+num_slow = int(num_songs * profile.get('slow'))
 
 
+#now we have a problem; the rounded number of each type of song
+#doesn't equal the total number of songs in the set. So we have
+#to deal with the overage
 
+shortfall = num_songs - (num_fast + num_medium + num_slow)
+
+if shortfall > 0:
+
+  for _ in xrange(shortfall):
+
+    tempos = ['fast', 'medium', 'slow']
+
+    random.shuffle(tempos)
+
+    if tempos[0] == 'fast':
+
+      num_fast = num_fast + 1
+
+    elif tempos[0] == 'medium':
+
+      num_medium = num_medium + 1
+
+    else:
+
+      num_slow = num_slow + 1
+
+
+#build a list of song tempos
 
 
 #==BUILD THE SETS==#
